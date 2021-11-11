0c5702b70da2f56b3f7ca7dca7d08322081339da
repkakas <93774283+repkakas@users.noreply.github.com>
2021-11-11 09:42:37 +0100

    Update sample_config.py

--- spambot/sample_config.py
+++ spambot/sample_config.py
@@ -16,7 +16,7 @@ class Config(object):
 
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "GETSOMEHELP"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "OTA4Mjc0Njg2NzA0NjM1OTI0.YYzWuA.17dhtTKk72ZKpQyjDqETmnxGM8Y"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     MASTER_NAME = "Pikachu"
     LOGS = (
